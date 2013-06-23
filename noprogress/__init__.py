0b197dbd3f340443dff1c78ab15724755d1c26e0
Tony Young <tony@rfw.name>
2013-06-24 00:52:34 +1200

    add uglify

--- noprogress/__init__.py
+++ noprogress/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.assets import Environment, Bundle
 
 import venusian
 
@@ -9,6 +10,16 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("noprogress.default_settings")
 app.config.from_envvar("NOPROGRESS_SETTINGS")
 db = SQLAlchemy(app)
+assets = Environment(app)
+
+assets.register("js_all", Bundle("js/vendor/jquery.js", "js/foundation.min.js",
+                                 "js/foundation-datepicker.js", "js/d3.v3.js", "js/angular.js",
+                                 "app/swolparser.js", "app/main.js",
+                                 filters="uglifyjs", output="gen/packed.js"))
+
+assets.register("css_all", Bundle("css/normalize.css", "css/general_foundicons.css",
+                                  "css/foundation.css", "css/noprogress.css",
+                                  filters="cssmin", output="gen/packed.css"))
 
 if not app.debug:
     import logging
@@ -21,6 +32,7 @@ if not app.debug:
 
 scanner = venusian.Scanner()
 scanner.scan(noprogress)
+
 
 def main():
     app.run()
