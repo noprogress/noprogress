e0780dfdc8481bec4eea6312421b8838c01b1afc
Tony Young <tony@rfw.name>
2013-06-24 01:12:24 +1200

    switch to uglifyjs2

--- noprogress/__init__.py
+++ noprogress/__init__.py
@@ -15,7 +15,7 @@ assets = Environment(app)
 assets.register("js_all", Bundle("js/vendor/jquery.js", "js/foundation.min.js",
                                  "js/foundation-datepicker.js", "js/d3.v3.js", "js/angular.js",
                                  "app/swolparser.js", "app/main.js",
-                                 filters="uglifyjs", output="gen/packed.js"))
+                                 filters="rjsmin", output="gen/packed.js"))
 
 assets.register("css_all", Bundle("css/normalize.css", "css/general_foundicons.css",
                                   "css/foundation.css", "css/noprogress.css",
